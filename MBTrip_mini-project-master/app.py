7ec7f01462a2bd143a825da7eb74f4e743a8f263
hayan-kim <greenhurb95@naver.com>
2022-05-12 09:31:25 +0900

    초기화, 재설정

--- MBTrip_mini-project-master/app.py
+++ MBTrip_mini-project-master/app.py
@@ -214,5 +214,5 @@ def like_review():
     return jsonify({'msg': '좋아요!'})
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=5001, debug=True)
+    app.run('0.0.0.0', port=5000, debug=True)
 
